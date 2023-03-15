331cd60d95aca54b80bd65ecb7e556c5344875db
Vitória Melo <127364360+vitoriaaahc@users.noreply.github.com>
2023-03-15 16:10:21 -0300

    Add files via upload

---
+++ Lógica de Programação/Multiplicar_arrow(atv1).js
@@ -0,0 +1,8 @@
+let multiplicar = (n1,n2) => {}
+const n1 = Number(prompt("Qual o primeiro valor?: "))
+const n2 = Number(prompt("Qual o segundo valor?:  "))
+
+const multiplica = (n1*n2) 
+
+multiplicar (multiplica)
+alert(`O valor da multiplicação é igual a: ${multiplica}`)
---
+++ Lógica de Programação/multiplicar_functiona(atv1).js
@@ -0,0 +1,4 @@
+console.log(multiplicar(5,6));
+function multiplicar(a,b){
+    return a*b;
+}
